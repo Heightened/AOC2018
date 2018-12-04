1e3da8090211154a905277e498e1187e0652b268
Carl <carl.vddh@gmail.com>
2018-12-04 01:46:17 +0100

    Add first part of Day1

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,5 +1,61 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
-int main() {
+class File {
+    std::ifstream fileStream;
+public:
+    File(char* fileName) : fileStream(std::ifstream(fileName)) {
+        std::cout << "Opening " << fileName << std::endl;
+    }
+
+	std::vector<std::string> getLines() {
+		std::vector<std::string> list;
+
+		std::string line;
+		while (true) {
+			std::getline(fileStream, line);
+
+			if (line != "") {
+				list.push_back(line);
+			}
+			else {
+				break;
+			}
+		}
+
+		return list;
+	}
+};
+
+class Parser1 {
+    int value;
+    
+public:
+	int getValue(std::string line) {
+		return std::stoi(line);
+	}
+
+    Parser1(File& file, int startingValue) : value(startingValue) {
+		std::vector<std::string> lines = file.getLines();
+
+		for (std::string line : lines) {
+			value += getValue(line);
+		}
+    }
+    
+    operator int() const { return value; }
+};
+
+int main(int argc, char** args) {
     std::cout << "Day1" << std::endl;
+    
+	char* fileName = "../input.txt";
+    if (argc > 1) {
+		fileName = args[1];
+	}
+
+    File file(fileName);
+    std::cout << std::to_string(Parser1(file, 0)) << std::endl;
 }
